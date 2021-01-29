d85eecf8eed0eb041f54c8d9d10d3b8af089befd
root <root@localhost>
2021-01-29 22:41:16 +0800

    add "中文编程.cpp" file

---
+++ c++/中文编程.cpp
@@ -0,0 +1,20 @@
+//程序需编译器支持中文
+#define 返回 return
+#define 输出 cout
+#define 整数 int
+#define 换行 endl
+#define 主函数 main
+#define 使用 using
+#define 命名空间 namespace
+#define 标准 std
+#define ； ;
+#include<iostream>
+
+
+整数 主函数()
+{
+	使用 命名空间 标准 ；
+	整数 a = 1 ；
+	输出 << a << 换行 ；
+	返回 0 ；
+}
