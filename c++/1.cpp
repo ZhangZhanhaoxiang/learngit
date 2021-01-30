2816ad8d1b06a2aa3160dd803906d34fd07ef511
root <root@localhost>
2021-01-30 08:01:07 +0800

    更改countPowMod函数

--- c++/1.cpp
+++ c++/1.cpp
@@ -1,29 +1,25 @@
-/*
- * bintmode.c :  此程序计算 (a^power) % mod. power 是大整数
- * 基本公式： (a*b) mod m = ((a mod m) * (b mod m)) mod m
- */
 #include<iostream>
-#include <stdio.h>
-#include <assert.h>
 using namespace std;
 
-int modMDyn(int, int ,int);
+int countPowMod(int, int ,int);
 
 int main()
 {
-   cout << modMDyn(451, 151, 15151) << endl;
-   cout << modMDyn(modMDyn(451, 151, 15151), 4639, 15151) <<endl;
+	int m, c, m_, e=157, n=15481, d=5045;
+	cout << "请输入明文m：" << endl;
+	cin >> m;
+	c = countPowMod(m, e, n);
+	cout << "密文c = " << c << endl;
+	m_ = countPowMod(c, d, n);
+	cout << "解密后的明文m_ = " << m_ << endl;
     
    return 0;
 }
-/*
- * modMDyn: 使用二进制分解技术求解 (a^power) % mod , power 是个大整数
- */
-int modMDyn(int a, int power, int mod)
+
+int countPowMod(int a, int power, int mod)
 {
-   assert(a>=1 && power >=0 && mod >=1);
-   int result = 1;
-   int temp = a % mod;
+   unsigned long long result = 1;
+   unsigned long long temp = a % mod;
    while (power >= 1) {
       if (power % 2 != 0) {
          result = (result * temp) % mod;   
