fe071edac46eb8e409a8fe64a286d6ba862752f0
zhang-zhanhaoxiang <zzhx13842727578@126.com>
2021-02-01 06:15:10 +0800

    add 数学金字塔-动态规划.cpp.

---
+++ c++/数字金字塔-动态规划.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+int a[1001][1001], n;
+int main()
+{
+    cin >> n;
+    for (int i=1; i<=n; i++)
+        {
+            for(int j=1; j<=i; j++)
+                {
+                    cin >> a[i][j];
+                }
+        }
+
+    for (int i=n-1; i>=1; i--)
+        {
+            for (int j=1; j<=i; j++)
+                {
+                    if (a[i+1][j]>=a[i+1][j+1])
+                        {
+                            a[i][j]+=a[i+1][j];
+                        }
+                    else
+                        {
+                            a[i][j]+= a[i+1][j+1];
+                        }
+                }
+        }
+
+    cout << a[1][1];
+    return 0;
+}
